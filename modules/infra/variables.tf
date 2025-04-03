0891e7badc850da38f1923d3fb3cbccf3eae3fd3
Arun Shekar <shekar.arun@gmail.com>
2025-04-03 11:55:51 +0530

    meta: Add validations for VPC CIDR variable

--- modules/infra/variables.tf
+++ modules/infra/variables.tf
@@ -1,4 +1,22 @@
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
+
+  validation {
+    condition     = can(cidrnetmask(var.vpc_cidr))
+    error_message = "The vpc_cidr value must be a valid CIDR notation"
+  }
+
+  validation {
+    condition     = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}/[0-9]{1,2}$", var.vpc_cidr))
+    error_message = "The vpc_cidr must be a valid IPv4 CIDR block (e.g., 10.0.0.0/16)"
+  }
+
+  validation {
+    condition = tonumber(
+      split("/", var.vpc_cidr)[1]) >= 16 && tonumber(split("/", var.vpc_cidr)[1]
+    ) <= 28
+    error_message = "The vpc_cidr prefix must be between /16 and /28"
+  }
 }
