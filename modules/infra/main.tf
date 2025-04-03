253f5b9ef7a46c796f755221d3ac0157e0287827
Arun Shekar <shekar.arun@gmail.com>
2025-04-03 13:58:42 +0530

    build: Add route table association with subnet

--- modules/infra/main.tf
+++ modules/infra/main.tf
@@ -43,3 +43,9 @@ resource "aws_subnet" "this" {
     Name = "${each.key}"
   }
 }
+
+resource "aws_route_table_association" "this" {
+  for_each       = aws_subnet.this
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.this.id
+}
