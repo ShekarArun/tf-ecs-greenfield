e5c171096a09eb3877326207129e841b9013fb82
Arun Shekar <shekar.arun@gmail.com>
2025-04-03 13:27:34 +0530

    feat: Allocate availability zones to subnets dynamically

--- modules/infra/main.tf
+++ modules/infra/main.tf
@@ -32,9 +32,12 @@ resource "aws_route" "this" {
 }
 
 resource "aws_subnet" "this" {
-  for_each   = { for i in range(var.num_subnets) : "ecs-public-subnet-${i + 1}" => i + 1 }
+  for_each   = { for i in range(var.num_subnets) : "ecs-public-subnet-${i}" => i }
   vpc_id     = aws_vpc.this.id
   cidr_block = cidrsubnet(aws_vpc.this.cidr_block, 8, each.value)
+  availability_zone = data.aws_availability_zones.available.names[
+    each.value % length(data.aws_availability_zones.available.names)
+  ]
 
   tags = {
     Name = "${each.key}"
