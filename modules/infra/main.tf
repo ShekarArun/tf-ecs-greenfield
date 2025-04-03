b943830c58b5fac6c777ab9b94fae55a57b8142f
Arun Shekar <shekar.arun@gmail.com>
2025-04-03 13:50:13 +0530

    refactor: Move AZ list to locals

--- modules/infra/main.tf
+++ modules/infra/main.tf
@@ -35,8 +35,8 @@ resource "aws_subnet" "this" {
   for_each   = { for i in range(var.num_subnets) : "ecs-public-subnet-${i}" => i }
   vpc_id     = aws_vpc.this.id
   cidr_block = cidrsubnet(aws_vpc.this.cidr_block, 8, each.value)
-  availability_zone = data.aws_availability_zones.available.names[
-    each.value % length(data.aws_availability_zones.available.names)
+  availability_zone = local.azs[
+    each.value % length(local.azs)
   ]
 
   tags = {
