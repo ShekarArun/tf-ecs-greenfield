fef1abd25cb6c4cfe0a504ee6eeb801b3e43abb7
Arun Shekar <shekar.arun@gmail.com>
2025-04-07 12:35:02 +0530

    meta: Fix output of app module to display multiple modules

--- modules/app/outputs.tf
+++ modules/app/outputs.tf
@@ -1,9 +1,9 @@
-# output "ecr_details" {
-#   description = "ECR Repository details"
-#   value = {
-#     name           = aws_ecr_repository.this.name
-#     arn            = aws_ecr_repository.this.arn
-#     registry_id    = aws_ecr_repository.this.registry_id
-#     repository_url = aws_ecr_repository.this.repository_url
-#   }
-# }
+output "ecr_details" {
+  description = "ECR Repository details"
+  value = {
+    name           = aws_ecr_repository.this.name
+    arn            = aws_ecr_repository.this.arn
+    registry_id    = aws_ecr_repository.this.registry_id
+    repository_url = aws_ecr_repository.this.repository_url
+  }
+}
