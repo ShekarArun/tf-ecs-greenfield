4da2f90dc951000cad1616e3bf7a3811e1e43553
Arun Shekar <shekar.arun@gmail.com>
2025-04-07 12:43:48 +0530

    build: Use lockfile when building

--- backends.tf
+++ backends.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tf-ecs-greenfield-tf-backend"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
+    bucket       = "tf-ecs-greenfield-tf-backend"
+    key          = "terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
   }
 }
